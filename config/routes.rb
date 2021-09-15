1b459e2e06b7439316688670a15cf2787b83b399
ishizukamaaya <maayajas@gmail.com>
2021-09-15 08:48:52 +0000

    Add routeing

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
   devise_for :members
 
-  resources :members,only:[:show, :edit, :update]
-  get 'menbers' => 'public/members#leave'
-  patch 'members' => 'public/members#withdraw'
+  namespace :public do
+    resources :members,only:[:show, :edit, :update]
+    get 'menbers' => 'public/members#leave'
+    patch 'members' => 'public/members#withdraw'
 
-  resources :products,only:[:index, :show]
-  root to :'public/homes#top'
-  get 'homes/about' => 'public/homes#about'
+    resources :products,only:[:index, :show]
+    root to :'public/homes#top'
+    get 'homes/about' => 'public/homes#about'
 
-  resources :cart_products,only:[:create, :update, :index, :destaroy]
-  delete 'cart_products' => 'public/cart_products#destaroy_all'
+    resources :cart_products,only:[:create, :update, :index, :destaroy]
+    delete 'cart_products' => 'public/cart_products#destaroy_all'
 
-  resources :orders,only:[:new, :create, :index, :show]
-  post 'orders' => 'public/orders#comfirm'
-  get 'orders' => 'public/orders#finish'
+    resources :orders,only:[:new, :create, :index, :show]
+    post 'orders' => 'public/orders#comfirm'
+    get 'orders' => 'public/orders#finish'
 
-  resources :addresses,only:[:index, :create, :destaroy, :edit, :update]
+    resources :addresses,only:[:index, :create, :destaroy, :edit, :update]
+  end
 
   namespace :admin do
     devise_for :admins
