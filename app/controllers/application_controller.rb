9b48ab6e7e5a4ee22846795fd1e65ca6bbe5244d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-09-16 08:07:57 +0000

    Devise need to be modifyed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  
+
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    public_root_path
+  end
+
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:password_confirmation])
   end
 end
+
--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,2 +1,18 @@
 class Public::CartProductsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+    CartProducts.all.destroy
+  end
 end
