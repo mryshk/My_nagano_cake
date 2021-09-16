2b5852dd622584711619670a15cf6e2254567845
EC2 Default User <ec2-user@ip-172-31-23-196.ec2.internal>
2021-09-16 03:37:03 +0000

    Add Order Index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,17 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_member!
+  
+  def index
+    @addresses = current_member.address
+    @address = Address.new
+  end
+  
+  
+  
+  private
+
+	def address_params
+  	params.require(:address).permit(:name, :postal_code, :address)
+  end
+  
 end
