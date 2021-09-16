97ef38197fc55facadc73fe20b865c98da114600
ishizukamaaya <maayajas@gmail.com>
2021-09-16 09:18:24 +0000

    add puroducts-new

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :genre
-  
+
   has_many :cart_products
   has_many :orders, through: :order_products
   has_many :order_products
+  attachment :image
 end
