b80e2a39f271b4dc02ef430d21c2bf89f844dd5b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-09-16 08:48:53 +0000

    Save

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,9 +1,16 @@
 class Public::CartProductsController < ApplicationController
 
   def index
+    @cart_products = CartProduct.all
   end
 
   def create
+    @cart_product = CartProduct.new
+    @product = Product.find(params[:id])
+    current_user.id = @cart_product.member_id
+    @product.id = @cart_product.product_id
+
+    @cart_product.save
   end
 
   def update
