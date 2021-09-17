be1d38b95ccf4d445070c4fd4af2ddbca0b923f1
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-09-17 09:08:06 +0000

    Add CartProduct

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -2,15 +2,19 @@ class Public::CartProductsController < ApplicationController
 
   def index
     @cart_products = CartProduct.all
+    @member_cart_products = CartProduct.where(member_id: current_member.id)
   end
 
   def create
-    @cart_product = CartProduct.new(cart_params)
+    @cart_product = CartProduct.new(create_cart_params)
     @cart_product.save
     redirect_to public_cart_products_path
   end
 
   def update
+    @cart_product = CartProduct.find(params[:id])
+    @cart_product.update(update_cart_params)
+    redirect_to public_cart_products_path
   end
 
   def destroy
@@ -25,7 +29,10 @@ class Public::CartProductsController < ApplicationController
   end
 
   private
-  def cart_params
-    params.permit(:product_id,:member_id)
+  def create_cart_params
+    params.permit(:product_id,:member_id,:quantity)
+  end
+  def update_cart_params
+    params.require(:cart_product).permit(:product_id,:member_id,:quantity)
   end
 end
