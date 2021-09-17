a1c8b0cb5bf73daf930f2ebcca3cbfa94eca3b99
EC2 Default User <ec2-user@ip-172-31-23-196.ec2.internal>
2021-09-17 11:00:34 +0000

    Add orders index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,13 +1,30 @@
 class Public::AddressesController < ApplicationController
 
+  before_action :authenticate_member!
 
   def index
-    
+    @addresses = current_member.address
+    @addresses = Address.all
     @address = Address.new
   end
- 
+
   def create
+    @address = Address.new(address_params)
+    @address.member_id = current_member.id
+    @addresses = current_member.address
+    if @address.save
+      redirect_to public_addresses_path
+    end
+  end
+
+  def destroy
+  end
+
+  def edit
     
+  end
+
+  def update
   end
 
   private
@@ -15,5 +32,6 @@ class Public::AddressesController < ApplicationController
 	def address_params
   	params.require(:address).permit(:name, :postal_code, :address)
   end
+
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,18 @@
 class Public::OrdersController < ApplicationController
+
+  def index
+     @orders = current_member.orders
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
+  end
+
+   private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_payment)
+  end
+
 end
