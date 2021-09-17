1d12b8bd8caab676fec075d7774cb7215f7579d7
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-09-17 14:43:58 +0000

    Save

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,8 +1,8 @@
 class Public::CartProductsController < ApplicationController
 
   def index
-    @cart_products = CartProduct.all
-    @member_cart_products = CartProduct.where(member_id: current_member.id)
+    @cart_products = CartProduct.all #カート内商品全て
+    @member_cart_products = CartProduct.where(member_id: current_member.id) #自分が入れたカート内商品全て
   end
 
   def create
@@ -19,20 +19,21 @@ class Public::CartProductsController < ApplicationController
 
   def destroy
     @cart_product = CartProduct.find(params[:id])
-    @cart_product.destroy
+    @cart_product.destroy #カート内商品1項目ずつ削除
     redirect_to public_cart_products_path
   end
 
   def destroy_all
-     CartProduct.destroy_all
+     CartProduct.destroy_all #カート内商品全て削除
      redirect_to public_cart_products_path
   end
 
   private
-  def create_cart_params
+
+  def create_cart_params #create用（カートに入れる用）のストロングパラメーター
     params.permit(:product_id,:member_id,:quantity)
   end
-  def update_cart_params
+  def update_cart_params #update用（個数更新用）のストロングパラメーター
     params.require(:cart_product).permit(:product_id,:member_id,:quantity)
   end
 end
--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -2,7 +2,8 @@ class Public::ProductsController < ApplicationController
 
   def index
     @genres = Genre.all
-    @products = Product.all
+    @all_products = Product.all #DB内の全ての商品（全？件の部分で必要なため）
+    @products = Product.page(params[:page]) #1ページに10項目ずつ取得
   end
 
   def show
